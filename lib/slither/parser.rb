4694acaab0683c14904ce819d6045a8c3490f59c
Bashir Eghbali <bashir@gmail.com>
2014-10-21 17:31:25 -0700

    added support for batched processing so you can parse in batches and process

--- lib/slither/parser.rb
+++ lib/slither/parser.rb
@@ -1,25 +1,37 @@
 class Slither
   class Parser
-        
+
     def initialize(definition, file_io)
       @definition = definition
       @file = file_io
       # This may be used in the future for non-linear or repeating sections
       @mode = :linear
     end
-    
-    def parse()
+
+    def batched_section_parse(parsed, row, line_number, batch_size, validate = false, &block)
+      @definition.sections.each do |section|
+        if section.match(row)
+          validate_length(row, section) if validate
+          parsed = fill_content(row, section, parsed)
+
+          if block_given? && line_number % batch_size == 0
+            yield parsed
+            parsed = {}
+          end
+        end
+      end
+    end
+
+    def parse(opts={}, &block)
       parsed = {}
+      batch_size = opts[:batch_size] || 0
+      line_number = 1
 
       @file.each_line do |line|
         line.chomp! if line
         next if line.empty?
-        @definition.sections.each do |section|
-          if section.match(line)
-            validate_length(line, section)
-            parsed = fill_content(line, section, parsed)
-          end
-        end
+        batched_section_parse(parsed, line, line_number, batch_size, true, &block)
+        line_number += 1
       end
 
       @definition.sections.each do |section|
@@ -27,52 +39,51 @@ class Slither
       end
       parsed
     end
-    
-    def parse_by_bytes
+
+    def parse_by_bytes(opts={}, &block)
       parsed = {}
-      
+      batch_size = opts[:batch_size] || 0
+      line_number = 1
+
       all_section_lengths = @definition.sections.map{|sec| sec.length }
       byte_length = all_section_lengths.max
       all_section_lengths.each { |bytes| raise(Slither::SectionsNotSameLengthError,
           "All sections must have the same number of bytes for parse by bytes") if bytes != byte_length }
-      
+
       while record = @file.read(byte_length)
-        
+
         unless remove_newlines! && byte_length == record.length
           parsed_line = parse_for_error_message(record)
-          raise(Slither::LineWrongSizeError, "Line wrong size: No newline at #{byte_length} bytes. #{parsed_line}") 
+          raise(Slither::LineWrongSizeError, "Line wrong size: No newline at #{byte_length} bytes. #{parsed_line}")
         end
-    
+
         record.force_encoding @file.external_encoding
-        
-        @definition.sections.each do |section|
-          if section.match(record)
-            parsed = fill_content(record, section, parsed)
-          end
-        end
+
+        batched_section_parse(parsed, record, line_number, batch_size, false, &block)
+        line_number += 1
       end
-      
+
       @definition.sections.each do |section|
         raise(Slither::RequiredSectionNotFoundError, "Required section '#{section.name}' was not found.") unless parsed[section.name] || section.optional
       end
       parsed
     end
-    
+
     private
-    
+
       def fill_content(line, section, parsed)
         parsed[section.name] ||= []
         parsed[section.name] << section.parse(line)
         parsed
       end
-      
+
       def validate_length(line, section)
         if line.length != section.length
           parsed_line = parse_for_error_message(line)
-          raise Slither::LineWrongSizeError, "Line wrong size: (#{line.length} when it should be #{section.length}. #{parsed_line})" 
+          raise Slither::LineWrongSizeError, "Line wrong size: (#{line.length} when it should be #{section.length}. #{parsed_line})"
         end
       end
-      
+
       def remove_newlines!
         return true if @file.eof?
         b = @file.getbyte
@@ -83,13 +94,13 @@ class Slither
           return false
         end
       end
-      
+
       def newline?(char_code)
         # \n or LF -> 10
         # \r or CR -> 13
         [10, 13].any?{|code| char_code == code}
       end
-      
+
       def parse_for_error_message(line)
         parsed = ''
         line.force_encoding @file.external_encoding
@@ -100,6 +111,6 @@ class Slither
         end
         parsed
       end
-      
+
   end
 end
--- lib/slither/slither.rb
+++ lib/slither/slither.rb
@@ -1,7 +1,7 @@
 class Slither
-  
+
   VERSION = '0.99.5'
-  
+
   class DuplicateColumnNameError < StandardError; end
   class RequiredSectionNotFoundError < StandardError; end
   class RequiredSectionEmptyError < StandardError; end
@@ -9,48 +9,48 @@ class Slither
   class ColumnMismatchError < StandardError; end
   class LineWrongSizeError < StandardError; end
   class SectionsNotSameLengthError < StandardError; end
-  
-  
+
+
   def self.define(name, options = {}, &block)
     definition = Definition.new(options)
     yield(definition)
     definitions[name] = definition
     definition
   end
-  
+
   def self.generate(definition_name, data)
     definition = definition(definition_name)
     raise ArgumentError, "Definition name '#{name}' was not found." unless definition
     generator = Generator.new(definition)
     generator.generate(data)
   end
-  
+
   def self.write(filename, definition_name, data)
     File.open(filename, 'w') do |f|
-      f.write generate(definition_name, data)  
+      f.write generate(definition_name, data)
     end
   end
-  
-  def self.parse(filename, definition_name)
+
+  def self.parse(filename, definition_name, opts={}, &block)
     raise ArgumentError, "File #{filename} does not exist." unless File.exists?(filename)
 
     file_io = File.open(filename, 'r')
-    parseIo(file_io, definition_name)
+    parseIo(file_io, definition_name, opts, &block)
   end
 
-  def self.parseIo(io, definition_name)
+  def self.parseIo(io, definition_name, opts={}, &block)
     definition = definition(definition_name)
     raise ArgumentError, "Definition name '#{definition_name}' was not found." unless definition
     parser = Parser.new(definition, io)
-    definition.options[:by_bytes] ? parser.parse_by_bytes : parser.parse
+    definition.options[:by_bytes] ? parser.parse_by_bytes(opts, &block) : parser.parse(opts, &block)
   end
-  
+
   private
-  
+
     def self.definitions
       @@definitions ||= {}
     end
-    
+
     def self.definition(name)
       definitions[name]
     end
